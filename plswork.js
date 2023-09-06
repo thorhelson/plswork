f520d3a4f82c5265e213aeb7bf26220b08b97354
thorhelson <104784435+thorhelson@users.noreply.github.com>
2023-09-06 18:47:38 +0100

    Create plswork.js

---
+++ plswork.js
@@ -0,0 +1,11 @@
+  // Get the banner-1YaD3N element
+  var banner1YaD3N = document.querySelector('.banner-1YaD3N');
+
+  // Get the bannerSVGWrapper-2CLfzN element
+  var bannerSVGWrapper = document.querySelector('.bannerSVGWrapper-2CLfzN');
+
+  // Check if banner-1YaD3N has a background image
+  if (window.getComputedStyle(banner1YaD3N)['background-image'] === 'none') {
+    // If no background image, hide bannerSVGWrapper
+    bannerSVGWrapper.style.display = 'none';
+  }
