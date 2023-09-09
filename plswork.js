5c02770de1cf7f173af99ba54a02be19067228ff
thorhelson <104784435+thorhelson@users.noreply.github.com>
2023-09-09 19:17:31 +0100

    Update plswork.js

--- plswork.js
+++ plswork.js
@@ -1,11 +1,15 @@
-  // Get the banner-1YaD3N element
-  var banner1YaD3N = document.querySelector('.banner-1YaD3N');
+  // Wait for the DOM to be fully loaded
+  document.addEventListener("DOMContentLoaded", function () {
+    // Get the banner-1YaD3N element
+    var banner1YaD3N = document.querySelector('.banner-1YaD3N');
 
-  // Get the bannerSVGWrapper-2CLfzN element
-  var bannerSVGWrapper = document.querySelector('.bannerSVGWrapper-2CLfzN')
+    // Get the bannerSVGWrapper-2CLfzN element
+    var bannerSVGWrapper = document.querySelector('.bannerSVGWrapper-2CLfzN');
 
-  // Check if banner-1YaD3N has a background image
-  if (window.getComputedStyle(banner1YaD3N)['background-image'] === 'none') {
-    // If no background image, hide bannerSVGWrapper
-    bannerSVGWrapper.style.display = 'none';
-  }
+    // Check if banner-1YaD3N has a background image
+    var style = window.getComputedStyle(banner1YaD3N);
+    if (style.getPropertyValue('background-image') === 'none' || style.getPropertyValue('background-image') === 'initial') {
+      // If no background image, hide bannerSVGWrapper
+      bannerSVGWrapper.style.display = 'none';
+    }
+  });
