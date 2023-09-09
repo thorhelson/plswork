f6262cf0a2e53c45a098a8c0d03d7e60e9285c67
thorhelson <104784435+thorhelson@users.noreply.github.com>
2023-09-09 20:13:50 +0100

    Update plswork.js

--- plswork.js
+++ plswork.js
@@ -6,10 +6,7 @@
     // Get the bannerSVGWrapper-2CLfzN element
     var bannerSVGWrapper = document.querySelector('.bannerSVGWrapper-2CLfzN');
 
-    // Check if banner-1YaD3N has a background image
-    var style = window.getComputedStyle(banner1YaD3N);
-    if (style.getPropertyValue('background-image') === 'none' || style.getPropertyValue('background-image') === 'initial') {
       // If no background image, hide bannerSVGWrapper
       bannerSVGWrapper.style.display = 'none';
-    }
+    
   });
